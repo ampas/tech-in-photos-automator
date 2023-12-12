fa80ac2853acfb26b3991548d988125b87bed2c7
Alex Forsythe <aforsythe@oscars.org>
2023-12-11 17:43:36 -0800

    improve script robustness
    - validate the xlsx file
       - looking for duplicate titles which will cause posting to fail
       - adjusting image urls for proper loading
       - checking that data is in required columns
       - reporting issues before trying to post

--- automator.py
+++ automator.py
@@ -1,6 +1,8 @@
 import requests
-import openpyxl
+import pandas as pd
 import json
+import argparse
+import re
 
 def create_topic(api_key, api_username, topic_title, raw_template, data):
     base_url = 'https://forums.techinphotos.com/'
@@ -26,7 +28,46 @@ def create_topic(api_key, api_username, topic_title, raw_template, data):
     else:
         print(f'Failed to create topic "{topic_title}"')
 
+def validate_data(df):
+    # Check for duplicate topic titles
+    duplicate_titles = df[df.duplicated(subset=['topic_title'], keep=False)]['topic_title']
+    if not duplicate_titles.empty:
+        print("Duplicate topic titles and their matching row numbers:")
+        duplicate_dict = {}
+        for title in duplicate_titles.unique():
+            matching_rows = df[df['topic_title'] == title]
+            row_numbers = matching_rows.index + 2
+            duplicate_dict[title] = row_numbers
+        for title, rows in duplicate_dict.items():
+            if len(rows) > 1:
+                print(f"- {title}: Rows {', '.join(map(str, rows[:-1]))}, and {rows[-1]} match")
+        return False
+
+    # Check for missing required columns
+    required_columns = ['project_id', 'image', 'id_num', 'library_desc', 'topic_title', 'search_terms']
+    missing_data = df[df[required_columns].isnull().any(axis=1)]
+
+    # Replace "https://www.dropbox.com" with "https://dl.dropboxusercontent.com" in string columns
+    string_columns = ['project_id', 'image', 'id_num', 'library_desc', 'topic_title', 'search_terms', 'subject_headings']
+    for col in string_columns:
+        df[col] = df[col].apply(lambda x: re.sub(r'https://www.dropbox.com', 'https://dl.dropboxusercontent.com', str(x)))
+
+    if not missing_data.empty:
+        print("Missing required data in the following rows:")
+        for index, row in missing_data.iterrows():
+            missing_columns = [col for col in required_columns if pd.isna(row[col])]
+            print(f"Row {index + 2}: Missing data in columns: {', '.join(missing_columns)}")
+        return False
+
+    return True
+
 def main():
+    parser = argparse.ArgumentParser(description='Process an Excel file.')
+    parser.add_argument('excel_file', help='Path to the Excel file')
+
+    # Parse arguments
+    args = parser.parse_args()
+
     # Read json configuration file with store api key details
     with open('config.json') as f:
         config = json.load(f)
@@ -38,31 +79,25 @@ def main():
     with open('template.md', 'r') as template_file:
         raw_template = template_file.read()
 
-    wb = openpyxl.load_workbook('test.xlsx')
-    sheet = wb.active
+    # Load the Excel file and specify dtype=str to read all data as strings
+    df = pd.read_excel(args.excel_file, dtype=str)
 
-    for row in sheet.iter_rows(min_row=2, values_only=True):  # skip the first row if it's a header
-        project_id, image, id_num, topic_title, library_desc, search_terms, subject_headings = row  # adjust the unpacking
+    # Validate the data before processing
+    if not validate_data(df):
+        return
 
-        # split the cell's content by line breaks, remove leading hyphens, and
-        # format as bullet points
-        if topic_title is None:
-            print("Skipping a row because the topic title is None.")
-            continue  # Skip the rest of this loop iteration and go to the next row
-        if subject_headings is None:
-            subject_headings = ''  # Use an empty string if the cell is empty
-        subject_headings = '\n'.join([f'* {line.lstrip("— ").strip()}' for line in subject_headings.split('\n') if line.strip()])
-
+    for index, row in df.iterrows():
         data = {
-            'project_id': project_id,
-            'image': image,
-            'id_num': id_num,
-            'library_desc': library_desc,
-            'topic_title': topic_title,
-            'search_terms': search_terms,
-            'subject_headings': subject_headings,
+            'project_id': row['project_id'],
+            'image': row['image'],
+            'id_num': row['id_num'],
+            'library_desc': row['library_desc'],
+            'topic_title': row['topic_title'],
+            'search_terms': row['search_terms'],
+            'subject_headings': row['subject_headings'],
         }
-        create_topic(api_key, api_username, topic_title, raw_template, data)
+
+        create_topic(api_key, api_username, row['topic_title'], raw_template, data)
 
 if __name__ == '__main__':
     main()
